8ce13649d34a353f86148c32b5f6748595a83cb6
Alessandra Vertrees <avertrees447@gmail.com>
2019-06-18 15:28:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,8 +46,9 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   hash[key]
     hash[key] += 1.
-    else tempHash[key]
-      tempHash[key] = 1
-    end
-  return tempHash
+  else hash[key]
+    hash[key] = 1
+  end
+  
+  return hash
 end
