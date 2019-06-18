4644059e996de900344ab402ff80be5f8f2d66e7
Alessandra Vertrees <avertrees447@gmail.com>
2019-06-18 15:11:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,12 @@
 def new_hash
   # return an empty hash
-  
+  newHash = {}
+  return newHash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  myHash = {}
 end
 
 def pioneer
